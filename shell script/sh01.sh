9cca9857c1444dfc54c9f4fce65e0a05c542d232
Tiande <dtdnqsb@163.com>
2015-05-26 23:04:53 +0800

    shell script

---
+++ shell script/sh01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Program:
+#   This program shows "Hello World!" in your screen.
+# History:
+# 2015/05/19    Tiadne  First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo -e "Hello World!\a \n"
+exit 0
