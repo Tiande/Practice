2bfc351b1760b44e971dc81386f26d1856bafca1
Tiande <dtdnqsb@163.com>
2015-06-01 16:39:38 +0800

    update python

--- python/pytest.py
+++ python/pytest.py
@@ -1,7 +1,46 @@
 #!/usr/bin/env python3
-a,b=0,1
+# -*- coding: utf-8 -*-
+#http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014316399410395f704750ee9440228135925a6ca1dad8000
 
-print(a+b+110)
+print('Hello, %s' % 'World')
+print('Hi, %s, you have $%d.' % ('Michael', 1000000),'\n')
 
-print('Hello World')
+print(len(b'ABC'))
+print(len(b'\xe4\xb8\xad\xe6\x96\x87'))
+print(len('中文'.encode('utf-8')),'\n')
 
+print(b'ABC'.decode('ascii'))
+print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'),'\n')
+
+# print('ABC'.encode('ascii'))
+# print('中文'.encode('utf-8'),'\n')
+
+# print(ord('A'),'\n')
+
+# print(chr(25991),'\n')
+
+# if 15 >= 18:
+	# print('True')
+# else:
+	# print('False','\n')
+	
+# print('''line1
+# line2
+# line3
+# end''','\n')
+
+# print('默认不转义方式： ',r'\\\\n\t ;)','\n')
+
+# print('\t','<--This is a tab','\n')
+
+# print('Test the \'Slash Single Quotes\'','\n')
+
+# print('1024 * 768 = ',1024*768,'\n')
+
+# a,b=0,1
+# print(a+b+110,'\n')
+
+# name = input('Type your name here: ')
+# print('Your name is: ',name,'\n')
+
+# print('Hello World','\n')
