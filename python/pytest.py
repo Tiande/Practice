b09a3d1f9581bdb5622f7176a908729ce61be424
Tiande <dtdnqsb@163.com>
2015-06-04 16:38:00 +0800

    merge

--- python/pytest.py
+++ python/pytest.py
@@ -2,20 +2,66 @@
 # -*- coding: utf-8 -*-
 #http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014316399410395f704750ee9440228135925a6ca1dad8000
 
-classmates = ['Bob','Jessey','Tommy']
-print(classmates,'\n','The len of this list is:',len(classmates),'\n',classmates[0],'\n',classmates[1],'\n',classmates[-1])
-classmates.append('Adam')
-print(classmates,'\n')
 
-print('Hello, %s' % 'World')
-print('Hi, %s, you have $%d.' % ('Michael', 1000000),'\n')
+names = ['Bob', 'Jany', 'Lucy']
+for name in names:
+	print(name)
+sum = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+	sum = sum + x
+	print(sum)
 
-print(len(b'ABC'))
-print(len(b'\xe4\xb8\xad\xe6\x96\x87'))
-print(len('中文'.encode('utf-8')),'\n')
+# age = int(input('input your age here: '))
+# if age >= 18:
+	# print('Your age is: ', age)
+	# print('adult')
+# elif age <= 6:
+	# print('Oh, baby')
+# else:
+	# print('teenager')
+	
+	
+# tuple
+# classmates = ('Michael', 'Bob', 'Tracy')
+# t = (1) # t = 1
+# print(t,'\n')
+# t = (1,) #tuple
+# print(t,'\n')
+# t = ('a','b',['A', 'B'])
+# print(t,'\n')
+# t[2][0] = 'X'
+# print(t,'\n')
 
-print(b'ABC'.decode('ascii'))
-print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'),'\n')
+
+# list
+# classmates = ['Bob','Jessey','Tommy']
+# print(classmates,'\n','The len of this list is:',len(classmates),'\n',classmates[0],'\n',classmates[1],'\n',classmates[-1])
+# classmates.append('Adam')
+# print(classmates,'\n')
+# classmates.insert(1, 'Jack')
+# print(classmates,'\n')
+# classmates.pop()
+# print(classmates,'\n')
+# classmates.pop(2)
+# print(classmates,'\n')
+# classmates[1] = 'Sarah'
+# print(classmates,'\n')
+# L = ['Apple', 123, True]
+# print(L,'\n')
+# s = ['python', 'java', ['asp', 'php'], 'scheme']
+# print(len(s),'\n')
+# print(s[2][1])
+
+
+# print('Hello, %s' % 'World')
+# print('Hi, %s, you have $%d.' % ('Michael', 1000000),'\n')
+
+# print(len(b'ABC'))
+# print(len(b'\xe4\xb8\xad\xe6\x96\x87'))
+# print(len('中文'.encode('utf-8')),'\n')
+
+# print(b'ABC'.decode('ascii'))
+# print(b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8'),'\n')
 
 # print('ABC'.encode('ascii'))
 # print('中文'.encode('utf-8'),'\n')
