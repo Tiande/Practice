5f8a26a5bf363df7406ec4ed128afba974cdce0f
Tiande <dtdnqsb@163.com>
2015-07-01 22:03:16 +0800

    youdao_TD.py

--- python/api/youdao_TD.py
+++ python/api/youdao_TD.py
@@ -15,67 +15,65 @@ import json
 import sys
 
 def getDic(args):
-    """getDic"""
     dic = ''
-    for item in sys.argv[1:slen+1]:
+    for item in sys.argv[1:argvLen+1]:
         dic += item + ' '
     return dic
 
-slen = len(sys.argv)
-dic = getDic(sys.argv) if slen>1 else input('请键入要查询的参数： \n')
+# 获取命令参数的长度
+argvLen = len(sys.argv)
+# 生成查询字符串
+dic = getDic(sys.argv) if argvLen>1 else input('请键入要查询的参数： \n')
+# 格式化请求地址
 uri =  'http://fanyi.youdao.com/openapi.do?keyfrom=' + keyfrom + '&key=' + APIkey + '&type=data&doctype=json&version=1.1&q=' + urllib.parse.quote(dic)
-content = json.loads(urllib.request.urlopen(uri).read().decode('utf-8', 'ignore')) # dict
+# 获取返回结果 dict
+content = json.loads(urllib.request.urlopen(uri).read().decode('utf-8', 'ignore'))
+# 获取翻译 list
+translation = content['translation'] if len(content['translation'])>0 else []
+# 获取基本词典 dict
+basic = content['basic'] if 'basic' in content else {}
+# 获取网络释义 list
+webValue = content['web'] if 'web' in content else ''
 
-def printDict(dic):
-    seq = '  '
-    for (key, value) in sorted(dic.items(), key=lambda dic: dic[0]): # .items() 转为元组数组 tuple; sorted 排序
-        seq += key + ' ==> '
-        if isinstance(value, list):
-            for item in value:
-                seq += item + ' '
-        else:
-            seq += value
-        seq += '\n  '
-    print(seq)
+space = '            '
 
-def printList(lst):
+# 打印翻译的方法
+def printTrans(lst):
     for value in lst:
         print('  ', value)
 
-def printWebValue(dic):
-    dic = dic.items()
-    seq = '  '
-    for (key, value) in sorted(dic, key=lambda dic: dic[0]):
-        if key=='key':
-            seq += value + ' ==> '
-        elif key=='value':
+# 打印基本词典的方法
+def printBasic(dic):
+    seq = ''
+    for (key, value) in sorted(dic.items(), key=lambda dic: dic[0]): # .items() 转为元组数组 tuple; sorted 排序
+        seq += '  ' + key + ':\n'
+        if isinstance(value, list):
             for item in value:
-                seq += item + ' '
+                seq += space  + item + '\n'
+        else:
+            seq += space + value + '\n'
     print(seq)
 
+# 打印网络释义的方法
+def printWebValue(lst):
+    for dic in lst:
+        dic = dic.items()
+        seq = ''
+        for (key, value) in sorted(dic, key=lambda dic: dic[0]):
+            if key=='key':
+                seq += '  ' +  value + '\n'
+            elif key=='value':
+                seq += space
+                for item in value:
+                    seq += item + ' '
+        print(seq)
 
-translation = content['translation'] if len(content['translation'])>0 else [] # 翻译 list
+# 显示结果
+print('\n翻译： ')
+printTrans(translation)
 
-def getTranslation(trans):
-    print('\n翻译： ')
-    printList(trans)
+print('\n基本词典： ')
+printBasic(basic)
 
-getTranslation(translation)
-
-basic = content['basic'] if 'basic' in content else {} # 基本词典 dict
-
-def getBasic(basic):
-    print('\n基本词典： ')
-    printDict(basic)
-
-getBasic(basic)
-
-webValue = content['web'] if 'web' in content else '' # 网络释义 list
-
-def getWebValue(webValue):
-    print('网络释义： ')
-    for value in webValue:
-        printWebValue(value)
-        # print(value)
-
-getWebValue(webValue)
+print('网络释义： ')
+printWebValue(webValue)
