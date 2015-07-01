a52dd27657d2fd99ec2a82c3470c2ddead43f88c
Tiande <dtdnqsb@163.com>
2015-07-01 21:20:39 +0800

    finally finish

--- python/api/youdao_TD.py
+++ python/api/youdao_TD.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# 感谢 网易有道翻译 ;)
+# 从下面的地址申请 APIkey && keyfrom
 # http://fanyi.youdao.com/openapi?path=data-mode
-# API key：242349121
-# keyfrom：Tiandechi
+
+APIkey = '242349121' # 你的 APIkey
+keyfrom = 'Tiandechi' # 你的 keyfrom
+__author__ = 'Tiande ( https://github.com/Tiande ) '
 
 import urllib.request
 import urllib.parse
@@ -19,9 +23,59 @@ def getDic(args):
 
 slen = len(sys.argv)
 dic = getDic(sys.argv) if slen>1 else input('请键入要查询的参数： \n')
+uri =  'http://fanyi.youdao.com/openapi.do?keyfrom=' + keyfrom + '&key=' + APIkey + '&type=data&doctype=json&version=1.1&q=' + urllib.parse.quote(dic)
+content = json.loads(urllib.request.urlopen(uri).read().decode('utf-8', 'ignore')) # dict
 
-uri =  'http://fanyi.youdao.com/openapi.do?keyfrom=Tiandechi&key=242349121&type=data&doctype=json&version=1.1&q=' + urllib.parse.quote(dic)
-content = json.loads(urllib.request.urlopen(uri).read().decode('utf-8', 'ignore'))
+def printDict(dic):
+    seq = '  '
+    for (key, value) in sorted(dic.items(), key=lambda dic: dic[0]): # .items() 转为元组数组 tuple; sorted 排序
+        seq += key + ' ==> '
+        if isinstance(value, list):
+            for item in value:
+                seq += item + ' '
+        else:
+            seq += value
+        seq += '\n  '
+    print(seq)
 
-for key in content:
-    print(key, ' : ', content[key])
+def printList(lst):
+    for value in lst:
+        print('  ', value)
+
+def printWebValue(dic):
+    dic = dic.items()
+    seq = '  '
+    for (key, value) in sorted(dic, key=lambda dic: dic[0]):
+        if key=='key':
+            seq += value + ' ==> '
+        elif key=='value':
+            for item in value:
+                seq += item + ' '
+    print(seq)
+
+
+translation = content['translation'] if len(content['translation'])>0 else [] # 翻译 list
+
+def getTranslation(trans):
+    print('\n翻译： ')
+    printList(trans)
+
+getTranslation(translation)
+
+basic = content['basic'] if 'basic' in content else {} # 基本词典 dict
+
+def getBasic(basic):
+    print('\n基本词典： ')
+    printDict(basic)
+
+getBasic(basic)
+
+webValue = content['web'] if 'web' in content else '' # 网络释义 list
+
+def getWebValue(webValue):
+    print('网络释义： ')
+    for value in webValue:
+        printWebValue(value)
+        # print(value)
+
+getWebValue(webValue)
