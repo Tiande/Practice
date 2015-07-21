75b0929a651b3884596c45f184ce7acb5c7fa5a4
Tiande <dtdnqsb@163.com>
2015-07-21 22:14:04 +0800

    new awesome web

--- python/awesome-python3-webapp/www/app.py
+++ python/awesome-python3-webapp/www/app.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import logging; logging.basicConfig(level=logging.INFO)
+__author__ = 'Tiande'
 
-import asyncio, os, json, time
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import asyncio
+import os
+import json
+import time
 from datetime import datetime
 
 from aiohttp import web
 
+
 def index(request):
     """docstring for index"""
     return web.Response(body=b'<h1>Awesome</h1>')
+
 
 @asyncio.coroutine
 def init(loop):
     """docstring for init"""
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
+    # 'loop.create_server()'利用 asyncio 创建 TCP 服务
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
